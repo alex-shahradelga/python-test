d56118478a17c26d2ba81229dcd17b72e736ec34
alex-shahradelga <fultdieyty734@hotmail.com>
2025-12-24 05:03:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-# Last updated: 2025-12-22 18:16:07
+# Last updated: 2025-12-24 05:03:23
 
 def fibonacci(n):    if n <= 1:        return n    return fibonacci(n-1) + fibonacci(n-2)if __name__ == "__main__":    for i in range(10):        print(f"F({i}) = {fibonacci(i)}")
